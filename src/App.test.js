3f90cc093649f81e0640845af7e2ea74be956fcf
KamrynR <62856359+KamrynR@users.noreply.github.com>
2020-08-14 17:53:00 -0500

    pr

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,29 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+
+test('renders first name', () => {
+    const { getByText } = render(<App />);
+    const firstName = getByText(/First Name/i);
+    expect(firstName).toBeInTheDocument();
+});
+
+test('renders last name', () => {
+    const { getByText } = render(<App />);
+    const lastName = getByText(/Last Name/i);
+    expect(lastName).toBeInTheDocument();
+});
+
+// test('adds name', () => {
+//     const {getByText, getByLabelText } = render(<App />);
+//     const input = getByLabelText('First Name*');
+//     fireEvent.change(input, { target : { register: 'Jett'}});
+
+//     const nameTest = getByText('Jett');
+//     expect(nameTest).toBeInTheDocument();
+// });
